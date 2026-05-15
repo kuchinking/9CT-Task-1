07e67a25fc941fb0c9b33d17e0ed1782fc9c529e
kuchinking <michael.kuchin3@education.nsw.gov.au>
2026-05-15 12:28:11 +1000

    Overhauled UI to work with a dictionary rather than variables.
    Made my UI work with the dictionary, currently set up 4 different menus with the dictionary. Simplifying the code.
    
    - Main menu
    - Datasets viewer
    - Datasets Selector
    - Datasets Selector - time

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ menu = "Introduction"
 valid = True
 
 
+
 menus = {
     "Main Menu": {
         "description": "Select an option:",
@@ -18,6 +19,7 @@ menus = {
         "selector2": "View visualisations",
         "selector3": "Update data",
         "selector4": "Quit program",
+        "selector5": "",
     },
     "Datasets Viewer": {
         "description": "Select an option, or look at the data:",
@@ -25,6 +27,7 @@ menus = {
         "selector2": "(2) View by date, quarter or year",
         "selector3": "(3) View a time period",
         "selector4": "(4) Go back to Main Menu",
+        "selector5": "",
     },
     "Datasets Selector": {
         "description": "Select an option:",
@@ -34,10 +37,26 @@ menus = {
         "selector4": "(4) View Australian inflation dataset",
         "selector5": "(5) Go back to Datasets Viewer",
     },
+    "Datasets Selector - Time": {
+        "description": "Select an option:",
+        "selector1": "(1) View specific year",
+        "selector2": "(2) View by quarter",
+        "selector3": "(3) View by year",
+        "selector4": "(4) Go back to Datasets Viewer",
+        "selector5": "",
+    },
+    "Datasets Selector - Time Period": {
+        "description": "",
+        "selector1": "(1) View specific year",
+        "selector2": "Enter the year",
+        "selector3": "(3) View by year",
+        "selector4": "(4) Go back to Datasets Viewer",
+        "selector5": "",
+    },
 }
 
 
-def user_UI():  # I recommend collapsing this
+def user_UI():  # I recommend collapsing this"selector5": "",
     global menu
     if menu == "Introduction":
         user_UI = f"""
@@ -108,13 +127,13 @@ Oil Prices vs Inflation - {menu}      Currently viewing: placeholder
 =============================================================================================================
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣶⣿⣿⣶⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠶⠶⢾⣿⣿⣿⣿⣧⣄⣀⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀    {menu} - {menu_description}
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠶⠶⢾⣿⣿⣿⣿⣧⣄⣀⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀    {menu} {menus[menu]["description"]}
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⡿⠛⠉⢙⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀    
-⠀⠀⠀⠀⣠⣴⣶⣶⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣾⣿⣶⣾⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀        (1) {selector1}
-⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀        (2) {selector2}
-⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⣠⣴⣿⣿⡿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀        (3) {selector3}
-⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣦⣤⣴⣶⣿⣿⠿⠛⠁⠀⠘⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀        (4) Go back to {previous_UI} 
-⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣮⣭⣽⡶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀        
+⠀⠀⠀⠀⣠⣴⣶⣶⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣾⣿⣶⣾⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀        {menus[menu]["selector1"]}
+⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀        {menus[menu]["selector2"]}
+⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⣠⣴⣿⣿⡿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀        {menus[menu]["selector3"]}
+⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣦⣤⣴⣶⣿⣿⠿⠛⠁⠀⠘⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀        {menus[menu]["selector4"]}
+⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣮⣭⣽⡶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀        {menus[menu]["selector5"]}
 ⠀⠀⢸⣿⣿⣿⣿⣿⣿⠋⠀⠈⠉⠛⠛⠿⠿⣿⣿⣇⣿⣿⣿⢿⣿⣿⣿⣟⣛⣯⣤⣤⣀⣀⡀⠀⠀⠀⠀⠀        
 ⠀⠀⠘⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⢻⣿⣿⡿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣤⡀
 ⠀⠀⠀⠹⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣾⣿⣿⣇⡄⠀⠀⠀⠉⠉⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⡇    To move through the UI write the corresponding
@@ -150,19 +169,13 @@ while not quit:
 
         if user_input == "1":
             valid = True
-            previous_UI = menu
             menu = "Datasets Viewer"
-            menu_description = "Select an option, or look at the data:"
-            selector1 = "Select dataset"
-            selector2 = "View date, quarter or year"
-            selector3 = "placeholder"
-            selector4 = "Go back to"
         elif user_input == "2":
             valid = True
-            menu = "Datasets Viewer"
+            menu = "Visualisations Viewer"
         elif user_input == "3":
             valid = True
-            menu = "Datasets Viewer"
+            menu = "Data Editor"
         elif user_input == "4":
             quit = True
         else:
@@ -177,15 +190,17 @@ while not quit:
         if user_input == "1":
             valid = True
             menu = "Datasets Selection"
-            menu_description = "Select an option:"
-            previous_UI = "Datasets Viewer"
-            selector1 = "view entire dataset"
-            selector2 = "view oil prices dataset (WTI)"
-            selector3 = "view Australian fuel price dataset"
+        elif user_input == "2":
+            valid = True
+            menu = "Datasets Selector - Time"
+        elif user_input == "2":
+            valid = True
+            menu = "Datasets Selector - Time"
         else:
             valid = False
     elif menu == "Datasets Selection":
         input()
+
 
 
 print("Quit successfully!")
